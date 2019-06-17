88ff682562112e1d3dc3e2bde514d17a28df0ecb
LaikaFusion <newrad4@gmail.com>
2019-06-17 16:47:08 -0400

    intial fetch query added, tested to console

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React,{useEffect} from "react"
+import React, { useEffect } from "react";
 
 const App = () => {
-	useEffect(() => console.log('mounted'), []);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+      });
+  }, []);
 
-	return (
-		<div>
-			<h1>Industry Weapon Applicant Test</h1>
-		</div>
-	)
-}
-export default App
+  return (
+    <div>
+      <h1>Industry Weapon Applicant Test</h1>
+    </div>
+  );
+};
+export default App;
