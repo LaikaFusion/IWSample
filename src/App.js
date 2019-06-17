d20f1629ae7ffbe4235b928d407d8f3c2b635625
LaikaFusion <newrad4@gmail.com>
2019-06-17 16:45:06 -0400

    intial install and use effect test added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from "react"
+import React,{useEffect} from "react"
 
 const App = () => {
+	useEffect(() => console.log('mounted'), []);
+
 	return (
 		<div>
 			<h1>Industry Weapon Applicant Test</h1>
